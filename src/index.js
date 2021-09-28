e40295311dfb3a352a4c840dc77d56bb90daad59
jazminmatos <jmatos1993@gmail.com>
2021-09-28 03:25:35 -0400

    populated w/ import statements

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './containers/App'
+import rootReduer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
