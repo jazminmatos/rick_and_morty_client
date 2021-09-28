0311b89e90ff499ebdc88862f0888c59dfedac71
jazminmatos <jmatos1993@gmail.com>
2021-09-28 03:46:37 -0400

    created functional component templates for navbar files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Router from '../components/Router'
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello from APP
+                <Router />
             </div>
         );
     }
